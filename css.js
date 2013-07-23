ee280790cebc5fd5f53cc79001fe1c017126024b
Ger Hobbelt <ger@hobbelt.com>
2013-07-23 17:04:14 +0200

    JSHint fixes.

--- css.js
+++ css.js
@@ -21,11 +21,11 @@
  * Chome 3 - 26
  * Firefox 3.5 - 19
  * Opera 10 - 12
- * 
+ *
  * browserling.com used for virtual testing environment
  *
  * Credit to B Cavalier & J Hann for the elegant IE 6 - 9 hack.
- * 
+ *
  * Sources that helped along the way:
  * - https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent
  * - http://www.phpied.com/when-is-a-stylesheet-really-loaded/
@@ -34,14 +34,27 @@
  */
 
 define('require/css', ['require/normalize'], function(normalize) {
-  function indexOf(a, e) { for (var i=0, l=a.length; i < l; i++) if (a[i] === e) return i; return -1; }
 
-  if (typeof window == 'undefined')
-    return { load: function(n, r, load){ load(); } };
+  function indexOf(a, e) {
+    for (var i = 0, l = a.length; i < l; i++) {
+      if (a[i] === e) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  if (typeof window === 'undefined') {
+    return {
+      load: function(n, r, load) {
+        load();
+      }
+    };
+  }
 
   // set to true to enable test prompts for device testing
   var testing = false;
-  
+
   var head = document.getElementsByTagName('head')[0];
 
   var engine = window.navigator.userAgent.match(/Trident\/([^ ;]*)|AppleWebKit\/([^ ;]*)|Opera\/([^ ;]*)|rv\:([^ ;]*)(.*?)Gecko\/([^ ;]*)|MSIE\s([^ ;]*)/);
@@ -49,7 +62,7 @@ define('require/css', ['require/normalize'], function(normalize) {
 
   if (!engine) {}
   else if (engine[1] || engine[7]) {
-    hackLinks = parseInt(engine[1]) < 6 || parseInt(engine[7]) <= 9;
+    hackLinks = parseInt(engine[1], 10) < 6 || parseInt(engine[7], 10) <= 9;
     engine = 'trident';
   }
   else if (engine[2]) {
@@ -61,22 +74,22 @@ define('require/css', ['require/normalize'], function(normalize) {
     // engine = 'opera';
   }
   else if (engine[4]) {
-    hackLinks = parseInt(engine[4]) < 18;
+    hackLinks = parseInt(engine[4], 10) < 18;
     engine = 'gecko';
   }
   else if (testing) {
     alert('Engine detection failed');
   }
-  
+
   //main api object
   var cssAPI = {};
 
   var absUrlRegEx = /^\/|([^\:\/]*:)/;
-  
+
   cssAPI.pluginBuilder = './css-builder';
 
   // used by layer builds to register their css buffers
-  
+
   // the current layer buffer items (from addBuffer)
   var curBuffer = [];
 
@@ -89,13 +102,16 @@ define('require/css', ['require/normalize'], function(normalize) {
   cssAPI.addBuffer = function(resourceId) {
     // just in case layer scripts are included twice, also check
     // against the previous buffers
-    if (indexOf(curBuffer, resourceId) != -1)
+    if (indexOf(curBuffer, resourceId) != -1) {
       return;
-    if (indexOf(bufferResources, resourceId) != -1)
+    }
+    if (indexOf(bufferResources, resourceId) != -1) {
       return;
+    }
     curBuffer.push(resourceId);
     bufferResources.push(resourceId);
-  }
+  };
+
   cssAPI.setBuffer = function(css, isLess) {
     var pathname = window.location.pathname.split('/');
     pathname.pop();
@@ -105,10 +121,12 @@ define('require/css', ['require/normalize'], function(normalize) {
     baseParts.pop();
     var baseUrl = baseParts.join('/') + '/';
     baseUrl = normalize.convertURIBase(baseUrl, pathname, '/');
-    if (!baseUrl.match(absUrlRegEx))
+    if (!baseUrl.match(absUrlRegEx)) {
       baseUrl = '/' + baseUrl;
-    if (baseUrl.substr(baseUrl.length - 1, 1) != '/')
+    }
+    if (baseUrl.substr(baseUrl.length - 1, 1) != '/') {
       baseUrl = baseUrl + '/';
+    }
 
     cssAPI.inject(normalize(css, baseUrl, pathname));
 
@@ -116,7 +134,7 @@ define('require/css', ['require/normalize'], function(normalize) {
     // clear the current buffer for the next layer
     // (just the less or css part as we have two buffers in one effectively)
     for (var i = 0; i < curBuffer.length; i++) {
-      // find the resources in the less or css buffer dependening which one this is
+      // find the resources in the less or css buffer depending which one this is
       if ((isLess && curBuffer[i].substr(curBuffer[i].length - 5, 5) == '.less') ||
         (!isLess && curBuffer[i].substr(curBuffer[i].length - 4, 4) == '.css')) {
         (function(resourceId) {
@@ -125,7 +143,7 @@ define('require/css', ['require/normalize'], function(normalize) {
 
           // set a short timeout (as injection isn't instant in Chrome), then call the load
           setTimeout(function() {
-            if (typeof onBufferLoad[resourceId] == 'function')
+            if (typeof onBufferLoad[resourceId] === 'function')
               onBufferLoad[resourceId]();
             // remove from onBufferLoad to indicate loaded
             delete onBufferLoad[resourceId];
@@ -136,18 +154,20 @@ define('require/css', ['require/normalize'], function(normalize) {
         curBuffer.splice(i--, 1);
       }
     }
-  }
+  };
+
   cssAPI.attachBuffer = function(resourceId, load) {
     // attach can happen during buffer collecting, or between injection and callback
     // we assume it is not possible to attach multiple callbacks
     // requirejs plugin load function ensures this by queueing duplicate calls
 
     // check if the resourceId is in the current buffer
-    for (var i = 0; i < curBuffer.length; i++)
+    for (var i = 0; i < curBuffer.length; i++) {
       if (curBuffer[i] == resourceId) {
         onBufferLoad[resourceId] = load;
         return true;
       }
+    }
 
     // check if the resourceId is waiting for injection callback
     // (onBufferLoad === true is a shortcut indicator for this)
@@ -161,7 +181,8 @@ define('require/css', ['require/normalize'], function(normalize) {
       load();
       return true;
     }
-  }
+    return false;
+  };
 
   var webkitLoadCheck = function(link, callback) {
     setTimeout(function() {
@@ -172,7 +193,7 @@ define('require/css', ['require/normalize'], function(normalize) {
       }
       webkitLoadCheck(link, callback);
     }, 10);
-  }
+  };
 
   var mozillaLoadCheck = function(style, callback) {
     setTimeout(function() {
@@ -182,7 +203,7 @@ define('require/css', ['require/normalize'], function(normalize) {
       } catch (e){}
       mozillaLoadCheck(style, callback);
     }, 10);
-  }
+  };
 
   // ie link detection, as adapted from https://github.com/cujojs/curl/blob/master/src/curl/plugin/css.js
   if (engine == 'trident' && hackLinks) {
@@ -201,13 +222,13 @@ define('require/css', ['require/normalize'], function(normalize) {
         head.appendChild(style);
       }
       ieLoadNextImport(style);
-    }
+    };
     var ieLoadNextImport = function(style) {
       var curImport = ieQueue.shift();
       if (!curImport) {
         style.onload = noop;
         ieStyles.push(style);
-        return;  
+        return;
       }
       style.onload = function() {
         curImport.cb(curImport.ss);
@@ -215,7 +236,7 @@ define('require/css', ['require/normalize'], function(normalize) {
       };
       var curSheet = style.styleSheet;
       curImport.ss = curSheet.imports[curSheet.addImport(curImport.url)];
-    }
+    };
   }
 
   // uses the <link> load method
@@ -225,11 +246,12 @@ define('require/css', ['require/normalize'], function(normalize) {
     link.rel = 'stylesheet';
     link.href = url;
     return link;
-  }
+  };
 
-  var noop = function(){}
+  var noop = function(){};
 
   cssAPI.linkLoad = function(url, callback) {
+    var link;
     var timeout = setTimeout(function() {
       if (testing) alert('timeout');
       callback();
@@ -240,16 +262,16 @@ define('require/css', ['require/normalize'], function(normalize) {
         link.onload = noop;
       // for style querying, a short delay still seems necessary
       setTimeout(callback, 7);
-    }
+    };
     if (!hackLinks) {
-      var link = createLink(url);
+      link = createLink(url);
       link.onload = _callback;
       head.appendChild(link);
     }
     // hacks
     else {
       if (engine == 'webkit') {
-        var link = createLink(url);
+        link = createLink(url);
         webkitLoadCheck(link, _callback);
         head.appendChild(link);
       }
@@ -263,7 +285,7 @@ define('require/css', ['require/normalize'], function(normalize) {
         ieLoad(url, _callback);
       }
     }
-  }
+  };
 
   /* injection api */
   var progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'];
@@ -275,24 +297,26 @@ define('require/css', ['require/normalize'], function(normalize) {
     }
 
     var xhr, i, progId;
-    if (typeof XMLHttpRequest !== 'undefined')
+    if (typeof XMLHttpRequest !== 'undefined') {
       xhr = new XMLHttpRequest();
-    else if (typeof ActiveXObject !== 'undefined')
+    }
+    else if (typeof ActiveXObject !== 'undefined') {
       for (i = 0; i < 3; i += 1) {
         progId = progIds[i];
         try {
           xhr = new ActiveXObject(progId);
         }
         catch (e) {}
-  
+
         if (xhr) {
           progIds = [progId];  // so faster next time
           break;
         }
       }
-    
-    xhr.open('GET', url, requirejs.inlineRequire ? false : true);
-  
+    }
+
+    xhr.open('GET', url, !requirejs.inlineRequire);
+
     xhr.onreadystatechange = function (evt) {
       var status, err;
       //Do not explicitly handle errors, those should be
@@ -311,9 +335,10 @@ define('require/css', ['require/normalize'], function(normalize) {
         }
       }
     };
-    
+
     xhr.send(null);
-  }
+  };
+
   //uses the <style> load method
   var styleCnt = 0;
   var curStyle;
@@ -324,12 +349,14 @@ define('require/css', ['require/normalize'], function(normalize) {
       head.appendChild(curStyle);
       styleCnt++;
     }
-    if (curStyle.styleSheet)
+    if (curStyle.styleSheet) {
       curStyle.styleSheet.cssText += css;
-    else
+    }
+    else {
       curStyle.appendChild(document.createTextNode(css));
-  }
-  
+    }
+  };
+
   // NB add @media query support for media imports
   var importRegEx = /@import\s*(url)?\s*(('([^']*)'|"([^"]*)")|\(('([^']*)'|"([^"]*)"|([^\)]*))\))\s*;?/g;
 
@@ -340,8 +367,9 @@ define('require/css', ['require/normalize'], function(normalize) {
   var loadCSS = function(fileUrl, callback, errback) {
 
     //make file url absolute
-    if (!fileUrl.match(absUrlRegEx))
+    if (!fileUrl.match(absUrlRegEx)) {
       fileUrl = '/' + normalize.convertURIBase(fileUrl, pathname, '/');
+    }
 
     get(fileUrl, function(css) {
 
@@ -363,7 +391,7 @@ define('require/css', ['require/normalize'], function(normalize) {
 
       // load the import stylesheets and substitute into the css
       var completeCnt = 0;
-      for (var i = 0; i < importUrls.length; i++)
+      for (var i = 0; i < importUrls.length; i++) {
         (function(i) {
           loadCSS(importUrls[i], function(importCSS) {
             css = css.substr(0, importIndex[i]) + importCSS + css.substr(importIndex[i] + importLength[i]);
@@ -376,24 +404,25 @@ define('require/css', ['require/normalize'], function(normalize) {
             }
           }, errback);
         })(i);
+      }
 
-      if (importUrls.length == 0)
+      if (importUrls.length === 0) {
         callback(css);
+      }
     }, errback);
-  }
+  };
 
-  
+
   cssAPI.normalize = function(name, normalize) {
     if (name.substr(name.length - 4, 4) == '.css')
       name = name.substr(0, name.length - 4);
-    
+
     return normalize(name);
-  }
-  
+  };
+
   var waitSeconds;
   var alerted = false;
   cssAPI.load = function(cssId, req, load, config, parse) {
-    
     waitSeconds = waitSeconds || config.waitSeconds || 7;
 
     var resourceId = cssId + (!parse ? '.css' : '.less');
@@ -404,7 +433,7 @@ define('require/css', ['require/normalize'], function(normalize) {
       return;
 
     fileUrl = req.toUrl(resourceId);
-    
+
     if (!alerted && testing) {
       alert(hackLinks ? 'hacking links' : 'not hacking');
       alerted = true;
@@ -416,22 +445,24 @@ define('require/css', ['require/normalize'], function(normalize) {
     else {
       loadCSS(fileUrl, function(css) {
         // run parsing after normalization - since less is a CSS subset this works fine
-        if (parse)
+        if (parse) {
           css = parse(css, function(css) {
             cssAPI.inject(css);
             setTimeout(load, 7);
           });
+        }
       });
     }
-  }
+  };
 
-  if (testing)
+  if (testing) {
     cssAPI.inspect = function() {
       if (stylesheet.styleSheet)
         return stylesheet.styleSheet.cssText;
       else if (stylesheet.innerHTML)
         return stylesheet.innerHTML;
-    }
-  
+    };
+  }
+
   return cssAPI;
 });
